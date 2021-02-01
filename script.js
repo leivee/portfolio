3083063df110d2498d1e4ed9c29d6b70ee0580a6
leivee <leikang.phan@gmail.com>
2021-02-01 18:50:22 -0500

    test pt 3

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ $(window).on(`load`, start);
 
 function loadpage(e) {
   e.preventDefault(); // helps it to stay wherever it's suppposed to stay
-  $("#menu a").removeClass("active");
+  $(`#menu a`).removeClass(`active`);
   $(this).addClass(`active`);
-  $(`main #content`).load(`about.html`);
+  $(`#content`).load(`about.html`);
 }
 
 $(document).on(`click`, `#menu a`, loadpage);
