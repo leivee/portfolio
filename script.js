34e42c607f7bb0e2c57cffc0995cb929b3ad40f3
leivee <leikang.phan@gmail.com>
2021-02-01 18:40:36 -0500

    portfolio
    testing

---
+++ script.js
@@ -0,0 +1,13 @@
+function start(){
+  $(`body`).css(`background-color`, `#DCDCDC`);
+}
+$(window).on(`load`, start);
+
+function loadpage(e) {
+  e.preventDefault(); // helps it to stay wherever it's suppposed to stay
+  $(`#menu a`).removeClass(`active`);
+  $(this).addClass(`active`);
+  $(`#content`).load(`about.html`);
+}
+
+$(document).on(`click`, `#menu a`, loadpage);
