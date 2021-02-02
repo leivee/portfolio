0333e7071b1d983d00cf1cf01f12dac9ac539a88
leivee <leikang.phan@gmail.com>
2021-02-01 19:40:15 -0500

    testing 2

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function loadpage(e) {
   e.preventDefault(); // helps it to stay wherever it's suppposed to stay
   $(`#menu a`).removeClass(`active`);
   $(this).addClass(`active`);
-  $(`#content`).load(`about.html`);
+
+  var href = $(this).attr(`href`)
+  $(`#content`).load(href);
 }
 
 $(document).on(`click`, `#menu a`, loadpage);
